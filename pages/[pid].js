73463f702ea33186ce75c2be69a8c3a2a00bfe30
Trong Hieu <tronghieu60s@gmail.com>
2021-09-17 13:57:40 +0700

    update contents

--- pages/[pid].js
+++ pages/[pid].js
@@ -1,31 +1,47 @@
-import { getAllPagesInSpace } from "notion-utils";
+import Head from "next/head";
+import { getAllPagesInSpace, getPageTitle } from "notion-utils";
 import { NotionRenderer } from "react-notion-x";
 import notion from "../core/notion";
 
-export const getStaticProps = async (context) => {
-  const pageId = context.params.pid;
-  const recordMap = await notion.getPage(pageId);
+const isDev = process.env.NODE_ENV === "development" || !process.env.NODE_ENV;
 
-  return { props: { recordMap } };
+export const getStaticProps = async (context) => {
+  const pid = context.params.pid;
+  const pageId = pid === "blog" ? process.env.NOTION_BLOG_PAGE : pid;
+  try {
+    const recordMap = await notion.getPage(pageId);
+    return { props: { recordMap } };
+  } catch (error) {
+    return { notFound: true };
+  }
 };
 
 export async function getStaticPaths() {
+  if (isDev) {
+    return { paths: [], fallback: true };
+  }
+
   const pages = await getAllPagesInSpace(
     process.env.NOTION_BLOG_PAGE,
     "",
     notion.getPage.bind(notion),
     { traverseCollections: false }
   );
-
-  console.log(Object.keys(pages));
-
   const paths = Object.keys(pages).map((pageId) => `/${pageId}`);
-
   return { paths, fallback: true };
 }
 
 export default function Blog({ recordMap }) {
+  if (!recordMap) {
+    return null;
+  }
+
   return (
-    <NotionRenderer recordMap={recordMap} fullPage={true} darkMode={false} />
+    <>
+      <Head>
+        <title>{getPageTitle(recordMap)}</title>
+      </Head>
+      <NotionRenderer recordMap={recordMap} fullPage={true} darkMode={false} />
+    </>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,9 @@
-import * as React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html lang="vi">
         <Head />
         <body>
           <div className="neno-container">
